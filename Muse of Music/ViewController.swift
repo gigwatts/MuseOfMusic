dc3b92f7dabb954f6cf8d301188d25eeeaa69ffc
Dustin Watts <dustin.b.watts@gmail.com>
2016-04-23 12:10:38 -0400

    Initial Commit

---
+++ Muse of Music/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Muse of Music
+//
+//  Created by Dustin Watts on 4/23/16.
+//  Copyright © 2016 Dustin Watts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
